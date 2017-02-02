eaa80382ab98056dbf8c49fd66b82ed5159de897
Miles P. Olson <mpolson64@gmail.com>
2017-02-01 21:57:28 -0800

    Added path to generate a message about the band

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 var genres = fs.readFileSync('genres.txt').toString().split('\n');
+var messages = fs.readFileSync('messages.txt').toString().split('\n');
 
 var router = express.Router();
 
@@ -32,6 +33,30 @@ app.get('/bandname', function(req, res) {
 app.get('/genre', function(req, res) {
     res.json({
 	genre: genres[Math.floor(Math.random() * genres.length)]
+    });
+});
+
+app.get('/full', function(req, res) {
+    var genre = genres[Math.floor(Math.random() * genres.length)]
+
+    var message = messages[Math.floor(Math.random() * messages.length)]
+
+    var script = spawn('python', ['generator.py']);
+    var dataString = '';
+    var bandName = '';
+
+    script.stdout.on('data', function(data) {
+	dataString += data.toString();
+    });
+
+    script.stdout.on('end', function() {
+	console.log(dataString);
+	bandName = dataString.substring(0, dataString.length - 1);
+	res.json({
+	    bandName: bandName,
+	    genre: genre,
+	    message: message.replace('%GENRE', genre).replace('%BAND', bandName)
+	});
     });
 });
 
