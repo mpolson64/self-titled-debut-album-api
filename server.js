bd1e4c3c3c8c3fb9af69f90101061d0f8f68b959
Miles P. Olson <mpolson64@gmail.com>
2017-02-01 21:15:45 -0800

    Added bandname path to API to generate a single band name

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+var spawn = require('child_process').spawn;
+
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
-
 var router = express.Router();
 
-app.listen(port);
-console.log('API open on port ' + port);
+app.get('/', function(req, res) {
+    res.json({
+	message: "Welcome to the Self Titled Debut Album API. For more information visit https://github.com/mpolson64/Self-Titled-Debut-Album-API"
+    });
+});
+
+app.get('/bandname', function(req, res) {
+    var script = spawn('python', ['generator.py']);
+    var dataString = '';
+
+    script.stdout.on('data', function(data) {
+	dataString += data.toString();
+    });
+
+    script.stdout.on('end', function() {
+	console.log(dataString);
+	res.json({
+	    bandName: dataString
+	});
+    });
+});
+
+var server = app.listen(process.env.PORT || 8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('API open on port ' + port);
+});
