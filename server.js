fa7ed28e2aa0b0d1933f64705dc79400fe70d0a0
Miles P. Olson <mpolson64@gmail.com>
2017-02-02 19:57:28 -0800

    Moved replace responsibility back to server now that glitch is fixed

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/full', function(req, res) {
 	res.json({
 	    bandName: bandName,
 	    genre: genre,
-	    message: message
+-	    message: message.replace('%GENRE', genre).replace('%BAND', bandName)
 	});
     });
 });
